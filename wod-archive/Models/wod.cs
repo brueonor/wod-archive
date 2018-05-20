0bb2999518363b01deaa499c07638ebe22f06cac
Alexandre Réhel <alexandre.rehel@hotmail.com>
2018-05-20 10:54:15 -0400

    initial commit

---
+++ wod-archive/Models/wod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wod_archive.Models
+{
+    public class Wod
+    {
+        public Guid Id { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public List<string> Score { get; set; } = new List<string>();
+
+    }
+}
