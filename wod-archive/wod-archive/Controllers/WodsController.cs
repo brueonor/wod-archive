90511a7747288feda040a32d0c50db869b9faacf
Alexandre Réhel <alexandre.rehel@hotmail.com>
2018-05-20 10:39:51 -0400

    initial commit

---
+++ wod-archive/wod-archive/Controllers/WodsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using wod_archive.Models;
+
+namespace wod_archive.Controllers
+{
+    public class WodsController : ApiController
+    {
+       
+        Wod[] wods = new Wod[]
+        {
+            new Wod{Id = Guid.NewGuid(), Titre = "Murph", Description="blabla"},
+            new Wod{Id = Guid.NewGuid(), Titre = "Fran", Description="faeafsdf"},
+            new Wod{Id = Guid.NewGuid(), Titre = "Grace", Description="23rwsedfasd"},
+        };
+
+        public IEnumerable<Wod> GetAllWods()
+        {
+            return wods;
+        }
+
+        public IHttpActionResult GetWod(Guid id)
+        {
+            var wod = wods.FirstOrDefault(x => x.Id == id);
+
+            if (wod == null)
+                return NotFound();
+
+            return Ok(wod);
+        }
+
+    }
+}
