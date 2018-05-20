a44f0193782cc1a6cf9776f20e00e46e3dc09e51
brueonor <alexandre.rehel@hotmail.com>
2018-05-20 11:04:21 -0400

    Setup solution (#1)
    * initial commit

---
+++ src/wod-archive/wod-archive/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Linq;
+
+namespace wod_archive
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                              .Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+                              "text/html",
+                              StringComparison.InvariantCultureIgnoreCase,
+                              true,
+                              "application/json"));
+        }
+    }
+}
